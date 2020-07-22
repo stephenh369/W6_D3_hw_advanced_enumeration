043317dcb85f28f1b14d8d9f0002b7fe1b035f81
Stephen <stephen_h369@live.co.uk>
2020-07-22 21:42:49 +0100

    all anagram tests pass

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -5,11 +5,7 @@ const AnagramFinder = function (word) {
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
     return otherWords.filter(word => {
        const anagram = word.toLowerCase().split('').every(letter => this.word.split('').includes(letter));
-       
        let result = anagram && this.word.length === word.length && this.word !== word;
-       
-       
-
        return result;
     });
 };
