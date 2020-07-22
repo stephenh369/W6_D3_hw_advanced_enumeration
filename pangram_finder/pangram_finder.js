6524005b132c1fb5e629adad027701af184136c1
Stephen <stephen_h369@live.co.uk>
2020-07-22 19:04:40 +0100

    pangram finder passes first 2 tests

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  return this.alphabet.every(letter => this.phrase.includes(letter));
+};
 
 module.exports = PangramFinder;
