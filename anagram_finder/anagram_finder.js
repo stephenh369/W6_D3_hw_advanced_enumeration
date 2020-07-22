c9b74bff2c2d83126cc8409ca864bb2d97f679e4
Stephen <stephen_h369@live.co.uk>
2020-07-22 21:20:34 +0100

    anagram first test passes

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,12 @@
 const AnagramFinder = function (word) {
-
-}
+    this.word = word;
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
-}
+    return otherWords.filter(word => {
+       return word.split('').every(letter => this.word.split('').includes(letter));
+        
+    });
+};
 
 module.exports = AnagramFinder;
--- anagram_finder/anagram_finder_spec.js
+++ anagram_finder/anagram_finder_spec.js
@@ -7,32 +7,32 @@ describe('AnagramFinder', function () {
     assert.deepStrictEqual(anagramFinder.findAnagrams(['cat', 'dog']), ['cat']);
   });
 
-  it('should be able to detect a non-anagram', function () {
+  xit('should be able to detect a non-anagram', function () {
     const anagramFinder = new AnagramFinder('potato');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['tomato']), []);
   })
 
-  it('should not detect words with too few letters as an anagram', function () {
+  xit('should not detect words with too few letters as an anagram', function () {
     const anagramFinder = new AnagramFinder('good');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['dog']), []);
   });
 
-  it('should not detect words with too many letters as an anagram', function () {
+  xit('should not detect words with too many letters as an anagram', function () {
     const anagramFinder = new AnagramFinder('dog');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['good']), []);
   });
 
-  it('should detect an anagram regardless of case', function () {
+  xit('should detect an anagram regardless of case', function () {
     const anagramFinder = new AnagramFinder('DeduCTionS');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['DiscOUnteD']), ['DiscOUnteD']);
   });
 
-  it('should not detect a word as it\'s own anagram', function () {
+  xit('should not detect a word as it\'s own anagram', function () {
     const anagramFinder = new AnagramFinder('javascript');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['javascript']), []);
   });
 
-  it('should not detect an empty string as an anagram', function () {
+  xit('should not detect an empty string as an anagram', function () {
     const anagramFinder = new AnagramFinder('word');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['']), []);
   });
