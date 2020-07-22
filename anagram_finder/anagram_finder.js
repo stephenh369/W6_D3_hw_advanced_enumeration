8413f98fc6cee668e7a52d1e99db4b0ffd13e2e6
Stephen <stephen_h369@live.co.uk>
2020-07-22 21:41:58 +0100

    not own word as anagram passes

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -5,7 +5,8 @@ const AnagramFinder = function (word) {
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
     return otherWords.filter(word => {
        const anagram = word.toLowerCase().split('').every(letter => this.word.split('').includes(letter));
-       let result = anagram && this.word.length === word.length;
+       
+       let result = anagram && this.word.length === word.length && this.word !== word;
        
        
 
